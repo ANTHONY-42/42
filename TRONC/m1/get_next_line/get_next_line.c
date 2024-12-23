a253242cc9bae37748e7f7721dc81e04c8480c86
ANTHONY-42 <anthony42paris@gmail.com>
2024-12-24 00:22:12 +0100

    dfew

--- TRONC/m1/get_next_line/get_next_line.c
+++ TRONC/m1/get_next_line/get_next_line.c
@@ -1,12 +1,40 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "libft.h"
+#include <string.h>
+#include <unistd.h>
+//#include "libft.h"
+
 #ifndef	BUFFER_SIZE
 #define	BUFFER_SIZE 5
+#endif
+
+char *strjoin(const char *s1, const char *s2) {
+    if (!s1 && !s2)
+        return NULL;
+    if (!s1)
+        return strdup(s2); // Si s1 est NULL, on retourne une copie de s2
+    if (!s2)
+        return strdup(s1); // Si s2 est NULL, on retourne une copie de s1
+
+    size_t len1 = strlen(s1);
+    size_t len2 = strlen(s2);
+
+    // Allouer suffisamment d'espace pour les deux chaînes + '\0'
+    char *result = malloc(len1 + len2 + 1);
+    if (!result)
+        return NULL;
+
+    // Copier s1 dans result
+    strcpy(result, s1);
+    // Ajouter s2 à la suite
+    strcat(result, s2);
+
+    return result;
+}
 
 // 	EXTRAIRE UNE LIGNE JUSQU'AU '\n'
-char	*trace_in_line(char *trace)
+char	*trace_x_line(char *trace)
 {
 	int	i;
 	char	*line;
@@ -37,7 +65,7 @@ char	*maj_trace(char	*trace)
 	int	i;
 	int	j;
 
-	char	*new_line;
+	char	*new_trace;
 	i = 0;
 	j = 0;
 	while (trace[i] && trace[i] != '\n')
@@ -47,7 +75,7 @@ char	*maj_trace(char	*trace)
 		free(trace);
 		return (NULL);
 	}
-	new_trace = malloc((ft_strlen(trace) - i + 1) * sizeof(char));
+	new_trace = malloc((strlen(trace) - i + 1) * sizeof(char));
 	if (!new_trace)
 		return (NULL);
 	i++; // passe le '\n'
@@ -61,27 +89,31 @@ char	*maj_trace(char	*trace)
 //	LIRE LES DONNEES AVEC READ;
 //	MAJ DE TRACE;
 //	EXTRAIRE UNE LIGNE
-char	*get_next_line(int fd)
+static char	*get_next_line(int fd)
 {
 	static char	*trace;
 	char 		*buffer;
 	char 		*line;
-	int		nbr_read;
+	int		nb_read;
+
 	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	
+
+	trace = malloc(1);
+	if (!trace)
+		return (NULL);
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
 
-	while (!ft_strchr(trace, '\n'))
+	while (!strchr(trace, '\n'))
 	{
 		nb_read = read(fd, buffer, BUFFER_SIZE); // rempli buffer
 		if (nb_read <= 0)
 			break;
 		buffer[nb_read] = '\0';
-		trace = ft_strjoin(trace, buffer); //cat buffer a la suite de trace
+		trace = strjoin(trace, buffer); //cat buffer a la suite de trace
 	}
 	free(buffer);
 	if (!trace || trace[0] == '\0')
