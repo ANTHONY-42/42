779ade7411b4e55d51362f28defebe644c485ac0
ANTHONY-42 <anthony42paris@gmail.com>
2025-04-05 13:14:28 +0200

    makefile

--- TRONC/m2/so_long/src/utils/free.c
+++ TRONC/m2/so_long/src/utils/free.c
@@ -12,13 +12,14 @@ void    error(char *str)
 	ft_putstr(str);
         exit(1);
 }
+
 int	close_game(t_game *game)
 {
-	if (game->win)
-		mlx_destroy_window(game->mlx, game->win);
+//	if (game->win)
+//		mlx_destroy_window(game->mlx, game->win);
 	if (game->mlx)
 	{
-		mlx_destroy_display(game->mlx);
+//		mlx_destroy_display(game->mlx);
 		free(game->mlx);
 	}
 	return (0);
