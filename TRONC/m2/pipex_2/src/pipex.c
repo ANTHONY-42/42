ac4de55ffe060942d8d3e130d3c038504c9069fd
Anthony Turtschi <anturtsc@made-f0Ar12s11.clusters.42paris.fr>
2025-04-15 19:16:43 +0200

    pipex

--- TRONC/m2/pipex_2/src/pipex.c
+++ TRONC/m2/pipex_2/src/pipex.c
@@ -1,3 +1,27 @@
 #include "pipex.h"
 
+void	pipex(int ac, char **av, char **envp)
+{
+	int	input;
+	int	output;
+	int	pipe_fd[2];
 
+	intput = open(av[1], O_RDONLY);
+	if (input < 0)
+		error("open input");
+	output = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (output < 0)
+		error("")
+
+	if (pipe(pipe_fd) == -1)
+		error("pipe");
+	
+	pid1 = fork();
+	if (pid1 == -1)
+		error("fork 1");
+	if (pid1 == 0)
+	{
+		dup2(pipe_fd[0], 0);
+		execve(av[2]);
+	}
+}
